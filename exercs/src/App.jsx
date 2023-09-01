f159f91437827cd1d496af0a667857d4721b76e1
ArthurFenilli <dincaradadooeste@gmail.com>
2023-09-01 16:59:01 -0300

    a

--- exercs/src/App.jsx
+++ exercs/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react"
 export default () => {
     return (
      <div className="flex">
-        <div className ={ `w-30 h-screen bg-Orange`}>MenuLateral</div>
+        <div className ={ `w-20 h-screen bg-Orange`}>
+        <img
+        src="./src/assets/polvo.png"
+        />
+        </div>
         <div className="p-7 text-2x1 font-semibold flex-1 h-screen">
             <h1>Pagina principal</h1>
         </div>
